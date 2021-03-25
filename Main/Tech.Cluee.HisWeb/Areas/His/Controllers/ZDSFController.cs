1556b01c8e7941c2ae48b5d3f3b57f05d26b8267
ClueeZhuo <clueezhuo@outlook.com>
2021-03-25 10:47:55 +0800

    FIX: 调整控制器

--- Main/Tech.Cluee.HisWeb/Areas/His/Controllers/ZDSFController.cs
+++ Main/Tech.Cluee.HisWeb/Areas/His/Controllers/ZDSFController.cs
@@ -5,6 +5,7 @@ using Tech.Cluee.HisData.Model;
 
 namespace Tech.Cluee.HisWeb.Areas.His.Controllers
 {
+    [HisArea]
     public class ZDSFController : EntityController<ZDSF>
     {
         static ZDSFController()
--- Main/Tech.Cluee.HisWeb/Areas/His/Controllers/ZYBHYZ0Controller.cs
+++ Main/Tech.Cluee.HisWeb/Areas/His/Controllers/ZYBHYZ0Controller.cs
@@ -5,6 +5,7 @@ using Tech.Cluee.HisData.Model;
 
 namespace Tech.Cluee.HisWeb.Areas.His.Controllers
 {
+    [HisArea]
     public class ZYBHYZ0Controller : EntityController<ZYBHYZ0>
     {
         static ZYBHYZ0Controller()
